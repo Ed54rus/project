9f8939c98b05f9979b55ea4dc6d77c9464658ea4
Eduard Malyshenko <ed54rus@mail.ru>
2022-02-08 16:56:23 +0700

    мелкие правки

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menuCloseBtn = document.querySelector('.menu-list-close-btn');
-let menuList = document.querySelector('.menu-list');
+const menuList = document.querySelector('.menu-list');
 
 menuBtn.onclick = function () {
   menuList.classList.toggle('active');
