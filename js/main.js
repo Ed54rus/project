256e6767578528e398bd9bb95fe74b8a7829967a
Eduard Malyshenko <ed54rus@mail.ru>
2022-02-08 22:20:17 +0700

    исправил замечния по 4 ДЗ

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menuCloseBtn = document.querySelector('.menu-list-close-btn');
-const menuList = document.querySelector('.menu-list');
+const menuListWrp = document.querySelector('.menu-list-wrp');
+const filterBtn = document.querySelector('.filter-btn');
+const filterBtnClose = document.querySelector('.filter-btn-close');
+const filterActive = document.querySelector('.filter-list-wrp');
 
 menuBtn.onclick = function () {
-  menuList.classList.toggle('active');
+  menuListWrp.classList.toggle('active');
 }
-
 menuCloseBtn.onclick = function () {
-  menuList.classList.remove('active');
+  menuListWrp.classList.remove('active');
+}
+filterBtn.onclick = function () {
+  filterActive.classList.add('filter-active');
+}
+filterBtnClose.onclick = function () {
+  filterActive.classList.remove('filter-active');
 }
